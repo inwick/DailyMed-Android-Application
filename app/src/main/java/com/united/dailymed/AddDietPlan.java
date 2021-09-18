34b3a37655b1f1942144f8253498e4bd100bc151
yasiruranathunga <yasirusr99@gmail.com>
2021-09-18 20:48:51 +0530

    Yasiru second commit

---
+++ app/src/main/java/com/united/dailymed/AddDietPlan.java
@@ -0,0 +1,14 @@
+package com.united.dailymed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDietPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diet_plan);
+    }
+}
