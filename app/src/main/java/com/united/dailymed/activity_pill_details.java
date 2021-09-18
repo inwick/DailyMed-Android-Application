c5674240ff2c7d076f184887ed971355e208a6eb
Indika <it20274252@my.sliit.lk>
2021-09-18 20:21:53 +0530

    2nd commit - Indika

---
+++ app/src/main/java/com/united/dailymed/activity_pill_details.java
@@ -0,0 +1,58 @@
+package com.united.dailymed;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class activity_pill_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill_details);
+
+        //Initialize And Assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.Pill);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.Heart:
+                        startActivity(new Intent(getApplicationContext(),Heart.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),Home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Fitness:
+                        startActivity(new Intent(getApplicationContext(),Fitness.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Pill:
+                        return true;
+                    case R.id.Water:
+                        startActivity(new Intent(getApplicationContext(),Water.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+
+        });
+
+    }
+}
