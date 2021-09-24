89d4fdc8dce23b15b60a70e28f100c75d85ddb13
Indika <it20274252@my.sliit.lk>
2021-09-24 12:01:48 +0530

    2nd commit - apiu 21 v2.0 Indika

--- app/src/main/java/com/united/dailymed/Pill.java
+++ app/src/main/java/com/united/dailymed/Pill.java
@@ -2,24 +2,56 @@ package com.united.dailymed;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.united.dailymed.Adapter.PillAdapter;
+import com.united.dailymed.Model.PillModel;
+import com.united.dailymed.Utils.DatabaseHandler;
 
-public class Pill extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Pill extends AppCompatActivity implements DialogCloseListener{
+
+    //define RecyclerView
+    private RecyclerView pillsRecyclerView;
+    private PillAdapter pAdapter;
+    private FloatingActionButton fab;
+
+    private List<PillModel> pillList;
+    private DatabaseHandler db;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pill);
+        setContentView(R.layout.activity_daily_pills);
+
 
         //Initialize And Assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        //Set Home Selected
+        //Set Pill Selected
         bottomNavigationView.setSelectedItemId(R.id.Pill);
 
         //Perform ItemSelectedListener
@@ -52,5 +84,46 @@ public class Pill extends AppCompatActivity {
 
         });
 
+
+        //data retrieving
+
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        pillList = new ArrayList<>();
+
+        pillsRecyclerView = findViewById(R.id.pillsRecyclerView);
+        pillsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        pAdapter = new PillAdapter(db,this);
+        pillsRecyclerView.setAdapter(pAdapter);
+
+        fab = findViewById(R.id.fab);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(pAdapter));
+        itemTouchHelper.attachToRecyclerView(pillsRecyclerView);
+
+        pillList = db.getAllPills();
+        //add new pill to top
+        Collections.reverse(pillList);
+        pAdapter.setPills(pillList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewPill.newInstance().show(getSupportFragmentManager(), AddNewPill.TAG);
+
+            }
+        });
+
     }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        pillList = db.getAllPills();
+        //add new pill details to top
+        Collections.reverse(pillList);
+        pAdapter.setPills(pillList);
+        pAdapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/com/united/dailymed/Utils/DatabaseHandler.java
@@ -0,0 +1,115 @@
+package com.united.dailymed.Utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.united.dailymed.Model.PillModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    //Database Name
+    private static final String NAME = "dailymed";
+    //Table Name
+    private static final String PILLS_TABLE = "pills";
+    private static final String ID = "id";
+    private static final String PILL = "pill";
+    private static final String PILLTIME = "pilltime";
+    private static final String STATUS ="status";
+    //Database creation quarry
+    private static final String CREATE_PILLS_TABLE = "CREATE TABLE " + PILLS_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PILL + " TEXT, " + PILLTIME + " TEXT, "
+            + STATUS + " INTEGER)";
+
+    //Create SQlite Database reference
+    private SQLiteDatabase db;
+
+    //Create Constructor
+    public DatabaseHandler(Context context){
+        super(context,NAME,null,VERSION);
+    }
+
+    //Create Table
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_PILLS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop the older tables
+        db.execSQL("DROP TABLE IF EXISTS  PILLS_TABLE");
+        //Create table again
+        onCreate(db);
+    }
+
+    //Get access to modify database
+    public void openDatabase(){
+        db = this.getWritableDatabase();
+    }
+
+    //insert Pills
+    public void insertPill(PillModel pill){
+        ContentValues cv = new ContentValues();
+        cv.put(PILL,pill.getPill());
+        cv.put(PILLTIME,pill.getPillTime());
+        cv.put(STATUS, 0);
+        db.insert(PILLS_TABLE, null, cv);
+    }
+
+    //get all details from database
+    public List<PillModel> getAllPills(){
+        List<PillModel> pillList = new ArrayList<>();
+        Cursor cur = null;
+        db.beginTransaction();
+        try{
+            cur = db.query(PILLS_TABLE, null,null,null,null,null,null,null);
+            if(cur != null){
+                if(cur.moveToFirst()){
+                    do{
+                        PillModel  pill = new PillModel();
+                        pill.setId(cur.getInt(cur.getColumnIndex(ID)));
+                        pill.setPill(cur.getString(cur.getColumnIndex(PILL)));
+                        pill.setPillTime(cur.getString(cur.getColumnIndex(PILLTIME)));
+                        pill.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
+                        pillList.add(pill);
+                    }while (cur.moveToNext());
+                }
+            }
+        }
+        finally {
+            db.endTransaction();
+            //assert cur != null;
+            cur.close();
+        }
+        return pillList;
+    }
+
+    public void updateStatus(int id,int status){
+        ContentValues cv = new ContentValues();
+        cv.put(STATUS,status);
+        db.update(PILLS_TABLE, cv, ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+    public void updatePill(int id, String pill){
+        ContentValues cv = new ContentValues();
+        cv.put(PILL,pill);
+        db.update(PILLS_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    public void updatePillTime(int id, String pilltime){
+        ContentValues cv = new ContentValues();
+        cv.put(PILLTIME,pilltime);
+        db.update(PILLS_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+
+    public void deletePill(int id){
+        db.delete(PILLS_TABLE, ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+}
