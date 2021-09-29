b3adab03e9fff01a9d78f8d6345bb9f18c71be11
Indika <it20274252@my.sliit.lk>
2021-09-29 21:06:11 +0530

    Pill Commint with Testing.

---
+++ app/src/main/java/com/united/dailymed/addPillDetails.java
@@ -0,0 +1,14 @@
+package com.united.dailymed;
+
+public class addPillDetails {
+    public String AddPills(String pillNAme) {
+        String pill = pillNAme;
+        return pill;
+    }
+
+    public int AddAmount(int amount) {
+        int amt = amount;
+        return amt;
+    }
+
+}
